46818a191bed5bbc5885a7b839cc6e97b83e0664
Charlie Jooste <charlie@dispairinggoose.co.uk>
2024-09-25 12:48:26 +0100

    initial

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using HangmanGame;
+
+try
+{
+    // Open the text file using a stream reader.
+    using var reader = new StreamReader("words_alpha.txt");
+
+    // Read the stream as a string.
+    string text = reader.ReadToEnd();
+
+    var words = text.Split('\n').Select(str => str.Trim()).ToArray();
+    var state = new State(words);
+       
+       while (state.HasLives())
+       {
+           state.Draw();
+           state.Process();
+       }
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
---
+++ HangmanGame/State.cs
@@ -0,0 +1,260 @@
+﻿namespace HangmanGame;
+
+public class State
+{
+    private readonly string[] Words;
+    
+    private readonly string _currentWord;
+    private List<char> _guessedLetters;
+    private int _lives;
+    
+    public State(string[] words)
+    {
+        Words = words;
+        _currentWord = Words[Random.Shared.Next(0, Words.Length)];
+        _guessedLetters = [];
+        
+        _lives = 5;
+    }
+    
+    public void Draw()
+    {
+            string[] hangmenIterations =
+            [
+
+                @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |     /|\  
+
+             |     / \  
+
+             |__________          
+
+ 
+
+             ",
+
+                @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |     /|  
+
+             |     / \  
+
+             |__________          
+
+ 
+
+             ",
+
+                @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |      |  
+
+             |     / \  
+
+             |__________          
+
+ 
+
+             ",
+
+                @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |      |  
+
+             |     /   
+
+             |__________          
+
+ 
+
+             ",
+
+                @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |      |  
+
+             |         
+
+             |__________          
+
+ 
+
+             ",
+
+                @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |       
+
+             |        
+
+             |__________          
+
+ 
+
+             ",
+
+                @"
+
+             --------
+
+             |      |   
+
+             |         
+
+             |       
+
+             |      
+
+             |__________          
+
+ 
+
+             "
+
+            ];
+
+ 
+
+ 
+
+            string hangman = @"
+
+             --------
+
+             |      |   
+
+             |      O   
+
+             |     /|\  
+
+             |     / \  
+
+             |__________          
+
+ 
+
+             ";
+
+            var picture = hangmenIterations[_lives];
+            Console.WriteLine(picture);
+
+            foreach (char letter in _currentWord)
+            {
+                if (!_guessedLetters.Contains(letter))
+                {
+                    Console.Write("_");
+                    
+                }
+                else
+                {
+                    Console.Write(letter);
+                                    }
+            }
+            
+            Console.WriteLine();
+    }
+
+    public void Process()
+    {
+        Console.WriteLine("Enter your guess: ");
+        
+        var playerInput = Console.ReadLine()!.ToLower();
+
+        if (playerInput.Length != 1)
+        {
+            Console.WriteLine("Please only write 1 letter");
+            Process();
+            return;
+        }
+
+        var input = playerInput[0];
+
+        if (!"qwertyuiopasdfghjklzxcvbnm".Contains(input))
+        {
+            Console.WriteLine("Please only write letters");
+            Process();
+            return;
+        }
+
+        bool valid = false;
+        
+        for (int i = 0; i < _currentWord.Length; i++)
+        {
+            if (input == _currentWord[i])
+            {
+                valid= true;
+                break;
+            }
+        }
+        
+        if (!valid)
+        {
+            _lives--;
+        }
+
+        _guessedLetters.Add(input);
+        Draw();
+
+        if (!HasLives())
+        {
+            Console.WriteLine("Game Over");
+            Console.WriteLine($"The word was");
+            Console.WriteLine(_currentWord);
+        } 
+        else if (HasWon())
+        {
+            Console.WriteLine("You won!");
+        }
+        else
+        {
+            Process();
+        }
+    }
+
+    public bool HasLives()
+    {
+        return _lives > 0;
+    }
+
+    public bool HasWon()
+    {
+        return _currentWord.All(c => _guessedLetters.Contains(c));
+    }
+}
